2bc70ab7880422eb86ecd5eb3b68c5e1fff5b493
fanfreak2247 <keandrewinston@gmail.com>
2017-12-23 17:02:26 -0500

    Update bubblesort.cpp

--- Bubble Sort/bubblesort.cpp
+++ Bubble Sort/bubblesort.cpp
@@ -7,13 +7,9 @@ Purpose: To sort an array of values using the bubble sorting algorithm.
 Environment: Code::Blocks 16.01 Windows 7 x64 GNU GCC Compiler
 */
 
-
 //Code uses a template so that you can use generic values. Goes through values of array n^n times and sorts values.
-
 template <class T>
-
 //The function uses references so it doesn't actually just copy the array, and changes what is inside it.
-
 void sort_arr(T& arr) {
      for (auto &a : arr) {
         for (auto &b: arr) {
@@ -21,12 +17,11 @@ void sort_arr(T& arr) {
                 std::swap(a,b);
         }
     }
-
-int main() {
-
+  }
+}
+int main() 
     std::array<int,6> boi = {4,4,5,3,2,1};
     sort_arr(boi);
-
     //Auto datatype just guesses type.
     for (auto a : boi) {
         std::cout << a << "\n";
